63d9d8fd67b9987693c2a6e026af8e74385312c8
Chase Bowden <chasebowden@Chases-MacBook-Pro.local>
2018-03-16 12:43:06 -0700

    add code to make initial tests pass

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -4,19 +4,25 @@ describe('Age', function() {
   let birthDate = "Wed Jan 29 1992"
   let userAge = new Age(birthDate)
   it('creates a new instance of the age class using the users birthdate', function() {
-    expect(userAge.birthdate.toDateString()).toEqual(birthDate)
+    expect(userAge.birthDate.toDateString()).toEqual(birthDate)
   });
   it('gives the users age in earth years ;)', function(){
     expect(userAge.earthYears()).toEqual(26)
   });
   it('gives us the users age in mercury years', function(){
-    expect(userAge.mercuryYears()).toEqual(26)
+    expect(userAge.mercuryYears()).toEqual(109)
   });
   it('gives us the users age in Venus years', function(){
-    expect(userAge.venusYears()).toEqual(26)
+    expect(userAge.venusYears()).toEqual(42)
+  });
+  it('gives us the users age in Mars years', function(){
+    expect(userAge.marsYears()).toEqual(14)
+  });
+  it('gives us the users age in Jupiter years', function(){
+    expect(userAge.jupiterYears()).toEqual(3)
   });
   it('determines how much longer a user has to live on each planet', function(){
-    
+
   });
 
 });
--- src/age.js
+++ src/age.js
@@ -1,11 +1,23 @@
 export class Age{
   constructor(birthDate){
-    this.birthdate = new Date (birthDate);
+    this.birthDate = new Date (birthDate);
   }
 
   earthYears(){
-    let years = 0;
-    return years;
+    let current = new Date(Date.now());
+    return current.getFullYear() - this.birthDate.getFullYear();
+  }
+  mercuryYears(){
+    return Math.ceil(this.earthYears()/.24);
+  }
+  venusYears(){
+    return Math.ceil(this.earthYears()/.62);
+  }
+  marsYears(){
+    return Math.ceil(this.earthYears()/1.88);
+  }
+  jupiterYears(){
+    return Math.ceil(this.earthYears()/11.86);
   }
 
 }
