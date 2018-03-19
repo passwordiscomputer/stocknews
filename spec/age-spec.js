19a94c2ee637359d28464793fe9762993b341936
Chase Bowden <chasebowden@Chases-MacBook-Pro.local>
2018-03-19 14:46:43 -0700

    refactor and rename age class. Now called person class.

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -1,43 +1,9 @@
-import {Age} from './../src/Age.js';
+import {Person} from './../src/person.js';
 
 describe('Age', function() {
-  let birthDate = "Wed Jan 29 1992"
-  let normalPersonAge = new Age(birthDate)
-  let oldPersonAge = new Age("Jan 29 1930")
-  it('creates a new instance of the age class using the users birthdate', function() {
-    expect(normalPersonAge.birthDate.toDateString()).toEqual(birthDate)
-  });
+  let person1 = new Person('Jan 29, 1992')
+
   it('gives the users age in earth years ;)', function(){
-    expect(normalPersonAge.earthYears()).toEqual(26)
-  });
-  it('gives us the users age in mercury years', function(){
-    expect(normalPersonAge.mercuryYears()).toEqual(109)
-  });
-  it('gives us the users age in Venus years', function(){
-    expect(normalPersonAge.venusYears()).toEqual(42)
-  });
-  it('gives us the users age in Mars years', function(){
-    expect(normalPersonAge.marsYears()).toEqual(14)
-  });
-  it('gives us the users age in Jupiter years', function(){
-    expect(normalPersonAge.jupiterYears()).toEqual(3)
-  });
-  it('determines how much longer a user has to live on earth', function(){
-    expect(normalPersonAge.earthExpectancy()).toEqual(78 - normalPersonAge.earthYears())
-  });
-  it('determines how long a person has lived past the proper age on earth', function(){
-    expect(oldPersonAge.earthExpectancy()).toEqual(78 - oldPersonAge.earthYears())
-  });
-  it('determines how much longer a user has to live on Mercury', function(){
-    expect(normalPersonAge.mercuryExpectancy()).toEqual(217)
-  });
-  it('determines how much longer a user has to live on Venus', function(){
-    expect(normalPersonAge.venusExpectancy()).toEqual(84)
-  });
-  it('determines how much longer a user has to live on mars', function(){
-    expect(normalPersonAge.marsExpectancy()).toEqual(28)
-  });
-  it('determines how much longer a user has to live on jupiter', function(){
-    expect(normalPersonAge.jupiterExpectancy()).toEqual(74)
+    expect(person1.age("Earth")).toEqual('On planet: Earth you are 26 years old.')
   });
 });
---
+++ src/person.js
@@ -0,0 +1,33 @@
+export class Person{
+  constructor(birthDate){
+    this.birthDate = new Date(birthDate);
+    this.current = new Date(Date.now());
+    this.baseAge =  this.current.getFullYear() - this.birthDate.getFullYear();
+
+  }
+  age(planet){
+    return (`On planet: ${planet} you are ${this.getYearsOnPlanet(planet, this.baseAge)} years old.`);
+  }
+
+  expectancy(lifeExpectancy, planet){
+    let remainingYears = lifeExpectancy - this.baseAge;
+    return remainingYears >= 0 ?
+    `On planet: ${planet} you have ${this.getYearsOnPlanet(planet, remainingYears)} years til your inevitable death` :
+    `On planet: ${planet} you have lived past your predetermined death by ${-(this.getYearsOnPlanet(planet, remainingYears))}`
+  }
+
+  getYearsOnPlanet(planet, years) {
+    switch (planet) {
+    case 'Mercury':
+      return years / 0.24;
+    case 'Venus':
+      return years / 0.62;
+    case 'Mars':
+      return years / 1.88;
+    case 'Jupiter':
+      return years / 11.86;
+    default:
+      return years;
+    }
+  }
+}
